e712bc63fa5d732036dada4a079f14dcc35f4f28
Oleg <kramo@ukr.net>
2017-03-04 21:01:32 +0200

    added note form and color picker

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
-import noteeditor from './noteeditor.jsx';
-import notesgrid from './notesgrid.jsx';
-const app = React.createClass({
+
+import NotesStore from '../stores/NotesStore';
+import NotesActions from '../actions/NotesActions';
+
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
+
+import './App.less';
+
+function getStateFromFlux() {
+    return {
+        isLoading: NotesStore.isLoading(),
+        notes: NotesStore.getNotes()
+    };
+}
+
+const App = React.createClass({
+    getInitialState() {
+        return getStateFromFlux();
+    },
+
+    componentWillMount() {
+        NotesActions.loadNotes();
+    },
+
+    componentDidMount() {
+        NotesStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount() {
+        NotesStore.removeChangeListener(this._onChange);
+    },
+
+    handleNoteDelete(note) {
+        NotesActions.deleteNote(note.id);
+    },
+
+    handleNoteAdd(noteData) {
+        NotesActions.createNote(noteData);
+    },
+
     render() {
         return (
-          <div className='App'>
+            <div className='App'>
                 <h2 className='App__header'>NotesApp</h2>
-                <NoteEditor/>
-                <NotesGrid/>
+                <NoteEditor onNoteAdd={this.handleNoteAdd} />
+                <NotesGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete} />
             </div>
-      );
+        );
+    },
+
+    _onChange() {
+        this.setState(getStateFromFlux());
     }
 });
 
-export default app;
+export default App;
--- client/components/note.jsx
+++ client/components/note.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const note = React.createClass({
+import './Note.less';
+
+const Note = React.createClass({
     render() {
-        return <h1> Note </h1>;
+        const style = { backgroundColor: this.props.color };
+
+        return (
+            <div className='Note' style={style}>
+                <span className='Note__del-icon' onClick={this.props.onDelete}> × </span>
+                {
+                    this.props.title
+                    ?
+                        <h4 className='Note__title'>{this.props.title}</h4>
+                    :
+                        null
+                }
+                <div className='Note__text'>{this.props.children}</div>
+            </div>
+        );
     }
 });
 
-export default note;
+export default Note;
